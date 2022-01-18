d7ed2956f82a144231a5724ee2e6f28660c40498
Angus McAloon <amcaloon@lcs.on.ca>
2022-01-18 09:27:57 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Angus McAloon on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
