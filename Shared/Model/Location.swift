fc3d0ab912a4f1a6e713620c01b3490dfe74e2dd
Angus McAloon <amcaloon@lcs.on.ca>
2022-01-18 09:52:16 -0500

    Added the Location Type

---
+++ Shared/Model/Location.swift
@@ -0,0 +1,30 @@
+//
+//  Location.swift
+//  TrekrMultiPlatform
+//
+//  Created by Angus McAloon on 2022-01-18.
+//
+
+import Foundation
+
+struct Location: Decodable {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let addvisory: String
+    
+    static let example = Location(id: 1,
+                                  name: "The Highlands",
+                                  country: "United Kingdom",
+                                  description: "The Highlands is a historic region of Scotland. Culturally, the Highlands and the Lowlands diverged from the later Middle Ages into the modern period, when Lowland Scots replaced Scottish Gaelic throughout most of the Lowlands. The term is also used for the area north and west of the Highland Boundary Fault, although the exact boundaries are not clearly defined, particularly to the east. The Great Glen divides the Grampian Mountains to the southeast from the Northwest Highlands. The Scottish Gaelic name of A' Ghàidhealtachd literally means \"the place of the Gaels\" and traditionally, from a Gaelic-speaking point of view, includes both the Western Isles and the Highlands.",
+                                  more: "The Great Smokies are part of an International Biosphere Reserve. The range is home to an estimated 187,000 acres (76,000 ha) of old growth forest, constituting the largest such stand east of the Mississippi River. The cove hardwood forests in the range's lower elevations are among the most diverse ecosystems in North America, and the Southern Appalachian spruce-fir forest that coats the range's upper elevations is the largest of its kind. The Great Smokies are also home to the densest black bear population in the Eastern United States and the most diverse salamander population outside of the tropics.",
+                                  latitude: 35.6532,
+                                  longitude: -83.5070,
+                                  heroPicture: "smokies",
+                                  addvisory: "We accept no liability for any visitors eaten alive by bears.")
+}
