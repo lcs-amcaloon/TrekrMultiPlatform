0c65f16016d482bddd4389110ad305737cbd7fb5
Angus McAloon <amcaloon@lcs.on.ca>
2022-01-19 10:11:59 -0500

    Got map view entered on north america

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Angus McAloon on 2022-01-19.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello TipsList View")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,29 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Angus McAloon on 2022-01-19.
+//
+import MapKit
+import SwiftUI
+
+struct WorldMap: View {
+    
+    // Centre on LCS, wide enough to show most of eastern North America
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.43922), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
+    var body: some View {
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
+        
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            WorldMap()
+        }
+    }
+}
